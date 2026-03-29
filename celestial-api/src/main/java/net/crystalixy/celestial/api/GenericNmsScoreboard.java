9305cedb0c38c423f02f1c96fbfc1eb2b9c951e0
Merry <svgr1808@outlook.de>
2026-03-29 15:24:26 +0200

    feat(api): add packet sending and objective lifecycle management to GenericNmsScoreboard

--- celestial-api/src/main/java/net/crystalixy/celestial/api/GenericNmsScoreboard.java
+++ celestial-api/src/main/java/net/crystalixy/celestial/api/GenericNmsScoreboard.java
@@ -2,23 +2,49 @@ package net.crystalixy.celestial.api;
 
 import io.papermc.paper.adventure.PaperAdventure;
 import net.kyori.adventure.text.Component;
+import net.minecraft.network.protocol.Packet;
+import net.minecraft.network.protocol.game.ClientboundSetObjectivePacket;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.scores.Objective;
 import net.minecraft.world.scores.Scoreboard;
 import net.minecraft.world.scores.criteria.ObjectiveCriteria;
+import org.bukkit.craftbukkit.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 
 public abstract class GenericNmsScoreboard {
+
+    public enum ObjectiveLifecycle {CREATE, REMOVE, UPDATE}
 
     private final Scoreboard scoreboard = new Scoreboard();
     private final String objectiveName;
     private Objective objective;
 
     private final Player player;
+    private boolean isDeleted;
 
     protected GenericNmsScoreboard(Player player, String objectiveName, Component title) {
         this.player = player;
         this.objectiveName = objectiveName;
-        this.objective = scoreboard.addObjective(
+        this.objective = createObjective(title);
+    }
+
+    protected void sendObjectivePacket(ObjectiveLifecycle lifecycle, Component title) {
+        if (lifecycle == ObjectiveLifecycle.CREATE) {
+            this.objective = createObjective(title);
+        }
+        var packet = new ClientboundSetObjectivePacket(objective, lifecycle.ordinal());
+        sendPacket(packet);
+    }
+
+    private void sendPacket(Packet<?> packet) {
+        if (isDeleted || !player.isOnline()) return;
+
+        ServerPlayer serverPlayer = ((CraftPlayer) player).getHandle();
+        serverPlayer.connection.send(packet);
+    }
+
+    private Objective createObjective(Component title) {
+        return scoreboard.addObjective(
                 objectiveName,
                 ObjectiveCriteria.DUMMY,
                 toVanillaComponent(title),
@@ -27,7 +53,7 @@ public abstract class GenericNmsScoreboard {
                 null);
     }
 
-    private static net.minecraft.network.chat.Component toVanillaComponent(Component component) {
+    private net.minecraft.network.chat.Component toVanillaComponent(Component component) {
         return component == null
                 ? net.minecraft.network.chat.Component.empty()
                 : PaperAdventure.asVanilla(component);
