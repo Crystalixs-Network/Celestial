f93617a79323874978a78ed58fd895fc5b879dfa
Merry <svgr1808@outlook.de>
2026-03-29 15:59:35 +0200

    feat(example): add ExamplePlugin with basic PlayerJoinEvent handling and scoreboard setup

---
+++ celestial-example/src/main/java/net/crystalixs/celestial/example/ExamplePlugin.java
@@ -0,0 +1,38 @@
+package net.crystalixs.celestial.example;
+
+import net.crystalixy.celestial.api.Scoreboard;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.List;
+
+import static net.kyori.adventure.text.Component.empty;
+import static net.kyori.adventure.text.Component.text;
+
+public final class ExamplePlugin extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @EventHandler
+    public void onJoin(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+
+        Scoreboard scoreboard = Scoreboard.sidebar()
+                .usePlayer(player)
+                .lines(List.of(
+                        empty(),
+                        text("Test"),
+                        empty(),
+                        empty(),
+                        text("Test2"),
+                        empty()
+                ))
+                .build();
+    }
+}
