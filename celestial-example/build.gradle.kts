f40a06bf05a39bb973269ad745bfbfdb0e1c1f83
Merry <svgr1808@outlook.de>
2026-03-29 13:56:21 +0200

    Add run-paper plugin and configure runServer task for Minecraft 1.21.11 development

--- celestial-example/build.gradle.kts
+++ celestial-example/build.gradle.kts
@@ -1,8 +1,15 @@
 plugins {
     java
+    alias(libs.plugins.runPaper)
 }
 
 dependencies {
     compileOnly(libs.paper)
     implementation(project(":celestial-api"))
+}
+
+tasks {
+    runServer {
+        minecraftVersion("1.21.11")
+    }
 }
