dfce6dc99cedb02d57a2de2629a503f6f67c654b
Merry <svgr1808@outlook.de>
2026-03-29 20:22:16 +0200

    ci(publish): add GitHub Actions workflow to publish Celestial API to GitHub Packages

--- celestial-api/build.gradle.kts
+++ celestial-api/build.gradle.kts
@@ -1,8 +1,34 @@
 plugins {
     `java-library`
+    `maven-publish`
     alias(libs.plugins.paperweight)
 }
 
 dependencies {
     paperweight.paperDevBundle("1.21.11-R0.1-SNAPSHOT")
+}
+
+java {
+    withSourcesJar()
+    withJavadocJar()
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+            artifactId = "celestial-api"
+        }
+    }
+
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/${System.getenv("GITHUB_REPOSITORY")}")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
 }
