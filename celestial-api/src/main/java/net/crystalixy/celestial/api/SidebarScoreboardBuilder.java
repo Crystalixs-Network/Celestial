2b8a78c268fe41a2e4adacd24758d8f66f58374e
Merry <svgr1808@outlook.de>
2026-03-29 16:50:55 +0200

    feat(api): implement sidebar scoreboard builder

--- celestial-api/src/main/java/net/crystalixy/celestial/api/SidebarScoreboardBuilder.java
+++ celestial-api/src/main/java/net/crystalixy/celestial/api/SidebarScoreboardBuilder.java
@@ -5,36 +5,82 @@ import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Collection;
+import java.util.Collections;
+import java.util.LinkedList;
+
+import static net.kyori.adventure.text.Component.empty;
 
 public final class SidebarScoreboardBuilder implements GenericScoreboard.Builder {
 
+    private final LinkedList<Component> lines = new LinkedList<>();
+    private final LinkedList<Component> scores = new LinkedList<>();
+    private Component title;
+    private Player player;
+
     @Override
     public GenericScoreboard.Builder title(@NotNull Component title) {
-        return null;
+        if (this.title != null) {
+            throw new IllegalStateException("Title is already set");
+        }
+
+        this.title = title;
+        return this;
     }
 
     @Override
     public GenericScoreboard.Builder emptyLine() {
-        return null;
+        line(empty());
+        return this;
     }
 
     @Override
     public GenericScoreboard.Builder line(@NotNull Component line) {
-        return null;
+        line(line, empty());
+        return this;
+    }
+
+    @Override
+    public GenericScoreboard.Builder line(@NotNull Component line, @NotNull Component score) {
+        lines.add(line);
+        scores.add(score);
+        return this;
     }
 
     @Override
     public GenericScoreboard.Builder lines(@NotNull Collection<@NotNull Component> lines) {
-        return null;
+        lines(lines, Collections.emptyList());
+        return this;
     }
 
     @Override
-    public GenericScoreboard.Builder usePlayer(Player player) {
-        return null;
+    public GenericScoreboard.Builder lines(@NotNull Collection<@NotNull Component> lines, @NotNull Collection<@NotNull Component> scores) {
+        if (lines.contains(null)) {
+            throw new IllegalArgumentException("Lines cannot contain null");
+        }
+
+        this.lines.addAll(lines);
+        if (scores.isEmpty()) {
+            for (int i = 0; i < lines.size(); i++) {
+                this.scores.add(empty());
+            }
+        } else {
+            this.scores.addAll(scores);
+        }
+
+        return this;
+    }
+
+    @Override
+    public GenericScoreboard.Builder withPlayer(Player player) {
+        this.player = player;
+        return this;
     }
 
     @Override
     public @NotNull GenericScoreboard build() {
-        return null;
+        if (player == null) throw new IllegalStateException("Player is not set");
+        if (title == null) throw new IllegalStateException("Title is not set");
+
+        return new SidebarScoreboard(player, title, lines, scores);
     }
 }
